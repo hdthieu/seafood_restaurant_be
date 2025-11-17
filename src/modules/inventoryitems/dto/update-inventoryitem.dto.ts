771d052909c480c82c5685b94dbba3c5a38c9a89
Hồ Đức Trí Hiếu <hoductrihieu@gmail.com>
2025-11-18 00:05:39 +0700

    task/BTS-179 | BTS-180 (add necessary ingredients) && BTS-182 | BTS-183 (New unit)

--- src/modules/inventoryitems/dto/update-inventoryitem.dto.ts
+++ src/modules/inventoryitems/dto/update-inventoryitem.dto.ts
@@ -1,4 +1,31 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateInventoryitemDto } from './create-inventoryitem.dto';
+// update-inventoryitem.dto.ts
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsArray, IsNumber, IsOptional, IsString, IsUUID } from 'class-validator';
 
-export class UpdateInventoryitemDto extends PartialType(CreateInventoryitemDto) {}
+export class UpdateInventoryitemDto {
+    @ApiPropertyOptional()
+    @IsOptional()
+    @IsString()
+    name?: string;
+
+    @ApiPropertyOptional()
+    @IsOptional()
+    @IsNumber()
+    alertThreshold?: number;
+
+    @ApiPropertyOptional()
+    @IsOptional()
+    @IsString()
+    description?: string | null;
+
+    @ApiPropertyOptional()
+    @IsOptional()
+    @IsUUID()
+    categoryId?: string | null;
+
+    @ApiPropertyOptional({ type: [String] })
+    @IsOptional()
+    @IsArray()
+    @IsUUID('all', { each: true })
+    supplierIds?: string[];
+}
