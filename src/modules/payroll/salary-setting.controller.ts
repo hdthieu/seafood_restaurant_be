c6fc7887ef40844250e5bc5877c3a2a82d85b054
dinhvanhung2003 <dinhhung12122003@gmail.com>
2025-12-11 15:48:59 +0700

    fix(payroll):employee rewards

--- src/modules/payroll/salary-setting.controller.ts
+++ src/modules/payroll/salary-setting.controller.ts
@@ -2,7 +2,13 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { PayrollService } from './payroll.service';
 import { CreateSalarySettingDto } from './dto/create-salary-setting.dto';
-
+import { UseGuards } from '@nestjs/common/decorators/core/use-guards.decorator';
+import { JwtAuthGuard } from '../core/auth/guards/jwt-auth.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { UserRole } from 'src/common/enums';
+import { RolesGuard } from '@modules/core/auth/guards/roles.guard';
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(UserRole.MANAGER)
 @Controller('salary-settings')
 export class SalarySettingController {
   constructor(private readonly payrollService: PayrollService) {}
